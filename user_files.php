62aa5cac3b9430484a9804699c7949d122d36971
Scriptonite <Scriptonite@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-15 15:24:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/user-files/trunk@345909 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- user_files.php
+++ user_files.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: User File Manager
 Plugin URI: http://www.whereyoursolutionis.com
-Description: Plugin to manage files for your users. You can upload files for your users to access, files uploaded to the user account are only viewable by the designated user.
+Description: Plugin to manage files for your users. You can upload files for your users to access, files upoloaded to the user account are only viewable by the designated user. Added options for user to add and/or delete files.
 Author: Innovative Solutions
-Version: 1.0.3
+Version: 1.0.4
 Author URI: http://www.whereyoursolutionis.com
 */
 
@@ -52,6 +52,8 @@ chmod($upload_dir['basedir'].'/file_uploads', 0777);
 
 add_option('file_manger_show_dash', 'yes');
 add_option('file_manger_show_menu', 'yes');
+add_option('file_manger_allow_del', 'no');
+add_option('file_manger_allow_up', 'no');
 
 
 
@@ -90,41 +92,95 @@ $isFolder = file_exists ($upload_dir['basedir'].'/file_uploads');
 }
 
 
+function Checkin_userfile_opts(){
 
+
+$currOpts_dash = get_option('file_manger_show_dash');
+$currOpts_menu = get_option('file_manger_show_menu');
+$currOpts_up = get_option('file_manger_allow_up');
+$currOpts_del = get_option('file_manger_allow_del');
+
+if($currOpts_dash and !$currOpts_up) {
+
+echo '<div id="message" class="updated">';
+echo 'New options have been added to the file manager options, you can change these settings <a href="'.ABSPATH.'/wp-admin/options-general.php?page=file_manager_options">here</a>';
+echo '</div>';
+
+}
+
+
+
+	if(!$CurrOpts_dash){
+
+	add_option('file_manger_show_dash', 'yes');
+	}
+
+	if(!$CurrOpts_menu){
+	add_option('file_manger_show_menu', 'yes');
+	}
+	if(!$CurrOpts_up){
+	add_option('file_manger_allow_del', 'no');
+	}
+	if(!$CurrOpts_del){
+	add_option('file_manger_allow_up', 'no');
+	}
+
+
+}
 
 
 
 
 	
-function files_settings_page() { ?>
-
+function files_settings_page() {
+Checkin_userfile_opts(); ?>
 <h2>File Manager Options</h2>
 <p>
-
+ 
 <?php if ($_GET['full_uninstall']==true) {
 DectivateFileDir();
 
 } 
 
+
+
 if ($_POST['update']) {
 
 $currOpts_dash = get_option('file_manger_show_dash');
 $currOpts_menu = get_option('file_manger_show_menu');
+$currOpts_up = get_option('file_manger_allow_up');
+$currOpts_del = get_option('file_manger_allow_del');
 
 	if ($_POST['file_manger_show_dash'] != $currOpts_dash ) {
 	
 		if($_POST['file_manger_show_dash']=='yes') {
 		update_option('file_manger_show_dash','yes' );
 		}else{
-		update_option('file_manger_show_dash','no' ); 
+		update_option('file_manger_show_dash','no' );
 		}
 	}
 	
-	if($_POST['file_manger_show_menu'] != $currOpts_menu ) { 
+	if($_POST['file_manger_show_menu'] != $currOpts_menu ) {
 		if($_POST['file_manger_show_menu']=='yes') {
 		update_option('file_manger_show_menu','yes' );
 		}else{
 		update_option('file_manger_show_menu','no' );
+		}
+	}
+	
+	if($_POST['file_manger_allow_del'] != $currOpts_del ) {
+		if($_POST['file_manger_allow_del']=='yes') {
+		update_option('file_manger_allow_del','yes' );
+		}else{
+		update_option('file_manger_allow_del','no' );
+		}
+	}
+	
+	if($_POST['file_manger_allow_up'] != $currOpts_up ) { 
+		if($_POST['file_manger_allow_up']=='yes') {
+		update_option('file_manger_allow_up','yes' );
+		}else{
+		update_option('file_manger_allow_up','no' );
 		}
 	}
 
@@ -134,6 +190,8 @@ $currOpts_menu = get_option('file_manger_show_menu');
 
 $currOpts_dash = get_option('file_manger_show_dash');
 $currOpts_menu = get_option('file_manger_show_menu');
+$currOpts_up = get_option('file_manger_allow_up');
+$currOpts_del = get_option('file_manger_allow_del');
 
  ?>
  <table class="form-table">
@@ -145,6 +203,12 @@ $currOpts_menu = get_option('file_manger_show_menu');
 	
 	<tr>
 	<td><input type="checkbox" name="file_manger_show_menu" value="yes" <?php if ($currOpts_menu== 'yes'){ echo 'checked'; }?>> Show Menu Item<br></td>
+    </tr>  
+	<tr>
+	<td><input type="checkbox" name="file_manger_allow_up" value="yes" <?php if ($currOpts_up== 'yes'){ echo 'checked'; }?>> Allow users to add files<br></td>
+    </tr>  
+	<tr>
+	<td><input type="checkbox" name="file_manger_allow_del" value="yes" <?php if ($currOpts_del== 'yes'){ echo 'checked'; }?>> Allow users to delete files<br></td>
     </tr>  
 	
 	<tr><td>
@@ -169,7 +233,7 @@ $currOpts_menu = get_option('file_manger_show_menu');
 }
 
 function manage_files_mainpg() {  
-
+Checkin_userfile_opts();
 
 $upload_dir = wp_upload_dir();
 
@@ -262,6 +326,7 @@ echo '</table>';
 
 
 function manage_files_upload() { 
+Checkin_userfile_opts();
 global $wpdb;
 echo '<p><h3>Upload Files</h3></p></p>';
 
@@ -339,15 +404,62 @@ $upload_dir = wp_upload_dir();
 global $current_user;
       get_currentuserinfo();
 
+if (isset($_GET['deletefile'])){
+
+$isitGone = unlink($_GET['deletefile']);
+
+		if ($isitGone) {
+
+		echo '<div id="message" class="updated">';
+		echo "The file has been deleted";
+		echo '</div>';
+		} else{
+		echo '<div id="message" class="error">';
+		echo "There was an error deleting the file, please try again!";
+		echo '</div>';
+		}
+}
+
+if (isset($_POST['addfiles'])){	
+			$target_path = $upload_dir['basedir'].'/file_uploads/'. $current_user->ID.'/';
+			
+			$target_path = $target_path . basename($_FILES['uploadedfile']['name']); 
+
+			if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+				echo '<div id="message" class="updated">';
+				echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
+				" has been uploaded";
+				echo '</div>';
+			} else{
+				echo '<div id="message" class="error">';
+				echo "There was an error uploading the file, please try again!<br />";
+				echo $php_errormsg;
+				echo '</div>';
+			}
+	}
+
+
+
 echo '<table class="widefat">';	
-echo'<thead><th>Your Files</th></thead>';
+echo'<thead><th>Your Files</th><th></th></thead>';
 	if ($handle = opendir($upload_dir['basedir'].'/file_uploads/'.$current_user->ID)) {
 		echo '<b><h5>Right click and select "save as" or "download" to save files</h5></b>';
 	while (false !== ($file = readdir($handle))) {
 			
 			if ($file!=".") {
 				if ($file!="..") {
-				echo '<tr><td><a href="'.$upload_dir['baseurl'].'/file_uploads/'.$current_user->ID .'/'.$file.'">'.$file .'</a></td></tr>';
+				echo '<tr><td><a href="'.$upload_dir['baseurl'].'/file_uploads/'.$current_user->ID .'/'.$file.'">'.$file .'</a></td>';
+				
+				$currOpts_up = get_option('file_manger_allow_del');
+				
+				if($currOpts_up=='yes') {
+				echo '<td><a href="admin.php?page=manage-files-user&deletefile='.$upload_dir['basedir'].'/file_uploads/'.$current_user->ID .'/'.$file.'">delete</a></td></tr>';
+				}else{
+				echo '<td></td></tr>';
+				
+				}//end if
+				
+				
 				}
 			}
 		}
@@ -356,6 +468,37 @@ echo'<thead><th>Your Files</th></thead>';
 	}
 
 echo '</table>';
+
+	$currOpts_up = get_option('file_manger_allow_up');
+
+		if ($currOpts_up=='yes'){  ?> 
+		<p><p><p><p><p>
+		
+		<table class="widefat"><tr><thead><th><h3>Add Files</h3></th></thead></tr>
+
+	<?php
+
+		$max_post = (int)(ini_get('post_max_size'));
+
+		$MaxSet=1000000*(int)$max_post;
+
+		?>
+
+		<tr><td>
+		<form enctype="multipart/form-data" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="POST">		
+		Choose a file to upload, your upload limit is <?php echo $max_post; ?>M <br />
+		
+		 <input name="uploadedfile" type="file" /><br />
+		 <input type="hidden" name="addfiles" value="addfiles" />
+		<input type="submit" value="Upload File" />
+		</form>
+		</td></tr>
+		<?php
+		echo '</table>';
+		}
+
+
+
 
 }
 
